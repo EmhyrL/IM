1a7e69f1ada383da2720ad10063aa921cb6bd452
EmhyrL <65173547+EmhyrL@users.noreply.github.com>
2021-06-10 19:58:25 +0800

    Add files via upload

---
+++ server/src/main/java/com/EmhyrL/IM/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.EmhyrL.IM.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class ServerApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
