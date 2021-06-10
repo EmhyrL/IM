0bf744d1fd33e56c84a467748ae63b067d26a8ff
EmhyrL <65173547+EmhyrL@users.noreply.github.com>
2021-06-10 19:56:35 +0800

    Add files via upload

---
+++ client/src/main/java/com/EmhyrL/IM/client/ClientApplication.java
@@ -0,0 +1,13 @@
+package com.EmhyrL.IM.client;
+
+/**
+ * Hello world!
+ *
+ */
+public class ClientApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
